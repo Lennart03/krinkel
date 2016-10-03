716891524b674431d93b02f54df326a283bed479
Wannes Van Dorpe <wannes.vandorpe@realdolmen.com>
2016-10-03 15:19:06 +0200

    Added ApplicationTest for main method

---
+++ Krinkel-Backend/src/test/java/com/realdolmen/chiro/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.realdolmen.chiro;
+
+import org.junit.Test;
+
+/**
+ * Created by WVDAZ49 on 3/10/2016.
+ */
+public class ApplicationTest {
+
+    @Test
+    public void testApplicationMain(){
+        Application application = new Application();
+        application.main(new String[0]);
+    }
+}
