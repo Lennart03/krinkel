a2f0402c3fc64c33949ac9064db7f622203e7d51
Wannes Van Dorpe <wannes.vandorpe@realdolmen.com>
2016-10-03 15:25:25 +0200

    added dud tests for configs

--- Krinkel-Backend/src/test/java/com/realdolmen/chiro/ApplicationTest.java
+++ Krinkel-Backend/src/test/java/com/realdolmen/chiro/ApplicationTest.java
@@ -1,15 +1,17 @@
 package com.realdolmen.chiro;
 
 import org.junit.Test;
+import org.springframework.boot.context.embedded.PortInUseException;
 
 /**
  * Created by WVDAZ49 on 3/10/2016.
  */
 public class ApplicationTest {
 
-    @Test
+    @Test(expected = PortInUseException.class)
     public void testApplicationMain(){
         Application application = new Application();
+        application.main(new String[0]);
         application.main(new String[0]);
     }
 }
---
+++ Krinkel-Backend/src/test/java/com/realdolmen/chiro/config/AppConfigTest.java
@@ -0,0 +1,14 @@
+package com.realdolmen.chiro.config;
+
+import org.junit.Test;
+
+/**
+ * Created by WVDAZ49 on 3/10/2016.
+ */
+public class AppConfigTest {
+
+    @Test
+    public void testConfigInit(){
+        AppConfig config = new AppConfig();
+    }
+}
