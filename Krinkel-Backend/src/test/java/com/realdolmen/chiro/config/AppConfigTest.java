991e7f68473c272ab1672ca2287efbf4203248f9
Ziggy Streulens <Ziggy.Streulens@realdolmen.com>
2016-10-04 10:26:44 +0200

    get basic user api working

--- Krinkel-Backend/src/test/java/com/realdolmen/chiro/config/AppConfigTest.java
+++ Krinkel-Backend/src/test/java/com/realdolmen/chiro/config/AppConfigTest.java
@@ -1,10 +1,13 @@
 package com.realdolmen.chiro.config;
 
 import org.junit.Test;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by WVDAZ49 on 3/10/2016.
  */
+
 public class AppConfigTest {
 
     @Test
---
+++ Krinkel-Backend/src/test/java/com/realdolmen/chiro/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package com.realdolmen.chiro.controller;
+
+
+import com.realdolmen.chiro.domain.User;
+import com.realdolmen.chiro.service.UserService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+
+    @InjectMocks
+    private UserController controller;
+
+    @Mock
+    private UserService userService;
+
+
+    @Test
+    public void getUserReturnsUserGivenByUserService(){
+        User u = new User("Ziggy", "test");
+        Mockito.when(userService.getUser("Ziggy", "test")).thenReturn(u);
+        Assert.assertSame(controller.getUser("Ziggy", "test"), u);
+    }
+}
