27b60c6d5966a2066e13dea9d46e368bf0fd3f34
Frank Claes <Frank.Claes@realdolmen.com>
2016-10-04 14:06:19 +0200

    modal panel & datepicker fixes

--- Krinkel-Frontend/src/main/webapp-dev/app/krinkel-register-participant/register-participant.component.js
+++ Krinkel-Frontend/src/main/webapp-dev/app/krinkel-register-participant/register-participant.component.js
@@ -1,6 +1,45 @@
 class RegisterParticipantController {
 
     constructor() {
+        /*var currentTime = new Date();
+        this.currentTime = currentTime;
+        this.month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        this.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        this.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        this.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+        this.disable = [false, 1, 7];
+        this.today = 'Today';
+        this.clear = 'Clear';
+        this.close = 'Close';
+        var days = 15;
+        this.minDate = (new Date(this.currentTime.getTime() - (1000 * 60 * 60 * 24 * days))).toISOString();
+        this.maxDate = (new Date(this.currentTime.getTime() + (1000 * 60 * 60 * 24 * days))).toISOString();
+        this.onStart = function () {
+            console.log('onStart');
+        };
+        this.onRender = function () {
+            console.log('onRender');
+        };
+        this.onOpen = function () {
+            console.log('onOpen');
+        };
+        this.onClose = function () {
+            console.log('onClose');
+        };
+        this.onSet = function () {
+            console.log('onSet');
+        };
+        this.onStop = function () {
+            console.log('onStop');
+        };
+*/
+        angular.element('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+    }
+    $onInit() {
+        angular.element('.modal-trigger').leanModal();
 
     }
 
@@ -8,7 +47,7 @@ class RegisterParticipantController {
 }
 
 
-export var RegisterParticipantComponent= {
+export var RegisterParticipantComponent = {
     template: require('./register-participant.html'),
     controller: RegisterParticipantController
 }
