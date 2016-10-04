f8e3cc0c12d7e84d108052788916c1665169fa9b
Philippe Desal <philippedesal@hotmail.com>
2016-10-04 14:03:32 +0200

    Validation being added

--- Krinkel-Frontend/src/main/webapp-dev/app/krinkel-register-participant/register-participant.component.js
+++ Krinkel-Frontend/src/main/webapp-dev/app/krinkel-register-participant/register-participant.component.js
@@ -1,6 +1,33 @@
 class RegisterParticipantController {
 
-    constructor() {
+    constructor($log) {
+        this.$log = $log;
+    }
+
+        addPerson(newPerson) {
+        var person = {
+            name: {
+                first: newPerson.firstName,
+                last: newPerson.lastName
+            },
+            email: newPerson.email
+        };
+        this.$log.debug(person);
+
+        newPerson.firstName = '';
+        newPerson.lastName = '';
+        newPerson.email = '';
+        newPerson.birthdate = '';
+        newPerson.street = '';
+        newPerson.building = '';
+        newPerson.postalCode = '';
+        newPerson.city = '';
+        newPerson.phone = '';
+        newPerson.email = '';
+        newPerson.gender = '';
+        newPerson = {};
+        this.addForm.$setPristine();
+
 
     }
 
