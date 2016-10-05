b698bfb39e7f3f9e4b29d0d05355550be73d6ce7
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-10-05 10:16:56 +0200

    connected scheduler to DB

--- Krinkel-Batch/src/main/java/com/realdolmen/chiro/batch/MemberSyncerService.java
+++ Krinkel-Batch/src/main/java/com/realdolmen/chiro/batch/MemberSyncerService.java
@@ -1,5 +1,8 @@
 package com.realdolmen.chiro.batch;
 
+import com.realdolmen.chiro.domain.RegistrationParticipant;
+import com.realdolmen.chiro.repository.RegistrationParticipantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +11,14 @@ import java.util.List;
 @Service
 public class MemberSyncerService {
 
-    @Scheduled(cron = "0/2 * * * * *")
+    @Autowired
+    private RegistrationParticipantRepository repository;
+
+    @Scheduled(cron = "0/10 * * * * *")
     public void executeBookProcessingSchedule() {
         System.out.println("Hello, World!");
+        List<RegistrationParticipant> all = repository.findAll();
+        System.out.println(all.size());
+
     }
 }
