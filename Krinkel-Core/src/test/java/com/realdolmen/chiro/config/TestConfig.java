a56493111457cb59ce8489318e54dcbaf7f88e29
Frank Claes <Frank.Claes@realdolmen.com>
2016-10-10 15:38:16 +0200

    gitignore: .classpath files added

--- Krinkel-Core/src/test/java/com/realdolmen/chiro/config/TestConfig.java
+++ Krinkel-Core/src/test/java/com/realdolmen/chiro/config/TestConfig.java
@@ -1,9 +1,7 @@
 package com.realdolmen.chiro.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 
 import com.realdolmen.chiro.service.EmailSenderService;
 import com.realdolmen.chiro.service.EmailSenderServiceImpl;
