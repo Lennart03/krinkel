3accff6baf494b5379869cb606e975258be5f512
HerrSubset <smets.pj@gmail.com>
2016-10-10 21:39:57 +0200

    add MultiSafePayService method to create link to payment page for participant

--- Krinkel-Multisafepay-Api/src/main/java/com/realdolmen/chiro/mspservice/MultiSafePayService.java
+++ Krinkel-Multisafepay-Api/src/main/java/com/realdolmen/chiro/mspservice/MultiSafePayService.java
@@ -1,11 +1,12 @@
 package com.realdolmen.chiro.mspservice;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.realdolmen.chiro.domain.RegistrationParticipant;
 import com.realdolmen.chiro.mspdto.OrderDto;
-import com.realdolmen.chiro.mspdto.OrderRequestDto;
 import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.security.InvalidParameterException;
 
 
 public class MultiSafePayService {
@@ -13,36 +14,72 @@ public class MultiSafePayService {
     private RestTemplate restTemplate = new RestTemplate();
 
 
-    public MultiSafePayService() {
-    }
-
-    public OrderDto createPayment() {
-        OrderRequestDto requestDto = new OrderRequestDto();
-        JSONObject paymentOptions = new JSONObject();
-
-        paymentOptions.put("notification_url", requestDto.getPayment_options().getNotification_url());
-        paymentOptions.put("redirect_url", requestDto.getPayment_options().getRedirect_url());
-        paymentOptions.put("cancel_url", requestDto.getPayment_options().getCancel_url());
-
-        JSONObject jsonObject = new JSONObject();
-
-        jsonObject.put("type", requestDto.getType());
-        jsonObject.put("order_id", requestDto.getOrder_id());
-        jsonObject.put("description", requestDto.getDescription());
-        jsonObject.put("currency", "EUR");
-        jsonObject.put("amount", requestDto.getAmount());
-        jsonObject.put("payment_options", paymentOptions);
+    /**
+     * Initiates a payment at Multisafepay.
+     *
+     * @param orderId the order id that is given to Multisafepay. This is used for later callbacks. For now, we consider
+     *                the ad-number of the person that is being paid for as order id.
+     *
+     * @param amount specifies the amount that has to be paid in eurocents
+     *
+     * @return returns the JSON response in object form (an OrderDto object)
+     */
+    public OrderDto createPayment(String orderId, Integer amount) {
+        if (!createPaymentParamsAreValid(orderId, amount)) throw new InvalidParameterException("cannot create a payment with those params");
+        JSONObject jsonObject = this.createPaymentJsonObject(orderId, amount);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<String>(jsonObject.toString(), headers);
+        HttpEntity<String> entity = new HttpEntity<>(jsonObject.toString(), headers);
 
         ResponseEntity<OrderDto> response= restTemplate.exchange(URL, HttpMethod.POST, entity, OrderDto.class);
 
-        OrderDto body = response.getBody();
-        System.out.println(response.getStatusCode());
-        System.out.println(body.toString());
-        System.out.println(body.getData().toString());
-        return body;
+        return response.getBody();
+    }
+
+    private boolean createPaymentParamsAreValid(String orderId, Integer amount) {
+        boolean res = true;
+
+        if (orderId == null)
+            res = false;
+
+        if (amount == null || amount < 0)
+            res = false;
+
+        return res;
+    }
+
+
+    /**
+     * Creates the JSON object that will be used in the request to initiate a multisafepay payment.
+     */
+    private JSONObject createPaymentJsonObject(String orderId, Integer amount) {
+        JSONObject paymentOptions = new JSONObject();
+
+        paymentOptions.put("notification_url", "https://krinkel.be/notify");
+        paymentOptions.put("redirect_url", "https://krinkel.be/success");
+        paymentOptions.put("cancel_url", "https://krinkel.be/failure");
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("type", "redirect");
+        jsonObject.put("order_id", orderId);
+        jsonObject.put("description", "this is the description");
+        jsonObject.put("currency", "EUR");
+        jsonObject.put("amount", amount);
+        jsonObject.put("payment_options", paymentOptions);
+
+        return jsonObject;
+    }
+
+
+    /**
+     * Returns the url that holds the payment page for the registration of a attendant of the camp.
+     * @param p the participant or volunteer
+     * @param amount the amount the participant or volunteer has to pay
+     * @return the url at which the payment page is located
+     */
+    public String getParticipantPaymentUri(RegistrationParticipant p, Integer amount) {
+        return this.createPayment(p.getAdNumber(), amount).getData().getPayment_url();
     }
 }
--- Krinkel-Multisafepay-Api/src/test/java/com/realdolmen/chiro/mspservice/MultiSafePayServiceTest.java
+++ Krinkel-Multisafepay-Api/src/test/java/com/realdolmen/chiro/mspservice/MultiSafePayServiceTest.java
@@ -1,14 +1,22 @@
 package com.realdolmen.chiro.mspservice;
 
 
+import com.realdolmen.chiro.domain.Gender;
+import com.realdolmen.chiro.domain.RegistrationParticipant;
+import com.realdolmen.chiro.domain.Role;
 import com.realdolmen.chiro.mspdto.OrderDto;
 import com.sun.org.apache.xerces.internal.util.URI;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.security.InvalidParameterException;
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = MultiSafePayService.class)
@@ -17,12 +25,41 @@ public class MultiSafePayServiceTest {
     @Autowired
     private MultiSafePayService multiSafePayService;
 
+    private RegistrationParticipant p;
+
+
+    @Before
+    public void init() {
+        p = new RegistrationParticipant("123", "jos@example.com", "Joske", "Vermeulen", new Date(), "AB 12/34", Gender.MAN, Role.ASPI, null);
+    }
+
 
     @Test
-    public void createPaymentReturnDtoWithUrl() throws URI.MalformedURIException {
-        OrderDto payment = multiSafePayService.createPayment();
+    public void createPaymentReturnDtoWithUrl(){
+        OrderDto payment = multiSafePayService.createPayment("abc", 100);
         Assert.assertNotNull(payment.getData().getPayment_url());
     }
+
+    @Test(expected = InvalidParameterException.class)
+    public void createPaymentThrowsErrorWhenOrderIdIsNull() {
+        multiSafePayService.createPayment(null, 100);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void createPaymentThrowsErrorWhenAmountIsNull() {
+        multiSafePayService.createPayment("abc", null);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void createPaymentThrowsErrorWhenAmountIsNegative() {
+        multiSafePayService.createPayment("abc", -1);
+    }
+
+    @Test
+    public void getParticipantPaymentUriReturnsUri() {
+        Assert.assertNotNull(multiSafePayService.getParticipantPaymentUri(p, 10000));
+    }
+
 
 
 
