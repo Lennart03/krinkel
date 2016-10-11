cc60103fc400c8e09c5779e64a81640235d6bae2
Mathias Bulté <bulte.mathias@gmail.com>
2016-10-11 12:00:52 +0200

    Fix import

--- Krinkel-Domain/src/main/java/com/realdolmen/chiro/domain/RegistrationVolunteer.java
+++ Krinkel-Domain/src/main/java/com/realdolmen/chiro/domain/RegistrationVolunteer.java
@@ -1,9 +1,6 @@
 package com.realdolmen.chiro.domain;
 
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Date;
