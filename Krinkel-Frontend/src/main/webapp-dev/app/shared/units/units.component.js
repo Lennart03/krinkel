b6f26b2cf4a3437f4ce20d5b493eef3813dbf3c9
Ziggy Streulens <ziggy.streulens@realdolmen.com>
2016-10-12 09:51:01 +0200

    previous button fixed

--- Krinkel-Frontend/src/main/webapp-dev/app/shared/units/units.component.js
+++ Krinkel-Frontend/src/main/webapp-dev/app/shared/units/units.component.js
@@ -10,11 +10,13 @@ class UnitsController {
     }
 
     openVerbond(verbond) {
-        this.verbond = verbond;
-        this.KrinkelService.getGewestenForVerbond(this.verbond.stamnummer).then((results) => {
+        console.log(verbond);
+        this.KrinkelService.getGewestenForVerbond(verbond.stamnummer).then((results) => {
+
             if (results.onderliggende_stamnummers != 0) {
-                this.unitLevel = this.verbond.naam;
+                this.unitLevel = verbond.naam;
                 this.verbonden = results.onderliggende_stamnummers;
+                this.verbond = results;
 
                 angular.forEach(this.verbonden, (value, index) => {
                     if (!value.stamnummer.endsWith("00")) {
@@ -25,6 +27,20 @@ class UnitsController {
                 });
             }
         });
+    }
+
+    goToPrevious(verbond) {
+
+        this.unitLevel = null;
+
+        if (verbond.bovenliggende_stamnummer != null) {
+            this.openVerbond(verbond.bovenliggende_stamnummer);
+        }
+        else {
+            this.KrinkelService.getVerbonden().then((results) => {
+                this.verbonden = results;
+            });
+        }
     }
 }
 
