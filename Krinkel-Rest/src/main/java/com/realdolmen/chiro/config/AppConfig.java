88764f197e07ee3a0525ea442f9a94e91865f986
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-10-18 17:29:55 +0200

    Work on spring configuration

--- Krinkel-Rest/src/main/java/com/realdolmen/chiro/config/AppConfig.java
+++ Krinkel-Rest/src/main/java/com/realdolmen/chiro/config/AppConfig.java
@@ -2,11 +2,10 @@ package com.realdolmen.chiro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 
 @Configuration
 public class AppConfig {
@@ -16,12 +15,12 @@ public class AppConfig {
      * TODO: Remove for production build
      */
     @Bean
+    @Profile("development")
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*");
-
             }
         };
     }
--- Krinkel-Rest/src/test/java/com/realdolmen/chiro/TestApplication.java
+++ Krinkel-Rest/src/test/java/com/realdolmen/chiro/TestApplication.java
@@ -1,18 +1,18 @@
 package com.realdolmen.chiro;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TestApplication {
 
+    // TODO: With the run method in comment all the tests still run.
+    // This means that this class is actually ignored.
+    // Hypothesis: Only the @Bean annotations like in a real config class are considered.
     public static void main(String[] args) {
         new SpringApplicationBuilder(TestApplication.class)
-                .bannerMode(Banner.Mode.OFF) // Well this seems to be ignored. That's totally fine.
-                                             // Good job Spring, go have some coffee...
-                                             // Then go and shoot yourself.
-                .run();
+                .bannerMode(Banner.Mode.OFF);
+                //.run();
     }
 }
