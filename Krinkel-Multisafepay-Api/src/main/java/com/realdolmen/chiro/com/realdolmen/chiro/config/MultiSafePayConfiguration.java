07278b54bd694a5cfed124437f39b59acf214870
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-10-18 16:21:43 +0200

    delete useless config

--- Krinkel-Multisafepay-Api/src/main/java/com/realdolmen/chiro/com/realdolmen/chiro/config/MultiSafePayConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.realdolmen.chiro.com.realdolmen.chiro.config;
-
-import org.springframework.boot.SpringBootConfiguration;
-
-@SpringBootConfiguration
-public class MultiSafePayConfiguration {
-}
