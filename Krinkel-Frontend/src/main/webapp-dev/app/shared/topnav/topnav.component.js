7b3e1e6d526ace0fbe354158b6a616f0771abf52
Arne Knockaert <arneknockaert@hotmail.com>
2016-10-24 15:49:44 +0200

    show name in topnav

--- Krinkel-Frontend/src/main/webapp-dev/app/shared/topnav/topnav.component.js
+++ Krinkel-Frontend/src/main/webapp-dev/app/shared/topnav/topnav.component.js
@@ -3,9 +3,17 @@ class TopNavController {
     constructor(AuthService, $location) {
         this.AuthService = AuthService;
         this.$location = $location;
+        this.userDetails;
     }
 
     $onInit() {
+    }
+
+    getUserDetails() {
+        this.AuthService.getUserDetails().then((resp) => {
+            this.userDetails = resp;
+        });
+        return this.userDetails;
     }
 }
 
