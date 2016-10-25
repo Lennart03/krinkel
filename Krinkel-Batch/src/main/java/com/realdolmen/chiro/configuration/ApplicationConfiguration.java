ef4b3d0b2b76a5d1bcb1c31cdcf2b04b1c455db3
Vincent Ceulemans <vincent.ceulemans@realdolmen.com>
2016-10-25 19:49:45 +0200

    Refactor tests in Krinkel-Batch

--- Krinkel-Batch/src/main/java/com/realdolmen/chiro/configuration/ApplicationConfiguration.java
+++ Krinkel-Batch/src/main/java/com/realdolmen/chiro/configuration/ApplicationConfiguration.java
@@ -6,9 +6,6 @@ import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
-/**
- * Created by PSTBB36 on 5/10/2016.
- */
 @EnableScheduling
 @Configuration
 public class ApplicationConfiguration {
