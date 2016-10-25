d66d4c659b2999d3a35134de358ce46c21ec0814
Vincent Ceulemans <vincent.ceulemans@realdolmen.com>
2016-10-25 21:13:03 +0200

    Move application.properties to separate module

--- Krinkel-Batch/src/main/java/com/realdolmen/chiro/configuration/ApplicationConfiguration.java
+++ Krinkel-Batch/src/main/java/com/realdolmen/chiro/configuration/ApplicationConfiguration.java
@@ -1,5 +1,6 @@
 package com.realdolmen.chiro.configuration;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -14,5 +15,15 @@ public class ApplicationConfiguration {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(20);
         return scheduler;
+    }
+
+    /**
+     * Set port explicitly. Will override default 8080 in application.properties.
+     */
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container -> {
+            container.setPort(8081);
+        });
     }
 }
