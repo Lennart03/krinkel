3feb040a0a91cd3ce356545fb1ec4dc8c36873b1
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-11-02 11:44:23 +0100

    reformat code

--- Krinkel-Core/src/main/java/com/realdolmen/chiro/service/ChiroContactService.java
+++ Krinkel-Core/src/main/java/com/realdolmen/chiro/service/ChiroContactService.java
@@ -1,20 +1,10 @@
 package com.realdolmen.chiro.service;
 
-import com.realdolmen.chiro.chiro_api.ChiroUserAdapter;
-import com.realdolmen.chiro.domain.User;
-import com.realdolmen.chiro.domain.units.ChiroUnit;
-import com.realdolmen.chiro.domain.units.RawChiroUnit;
-import com.realdolmen.chiro.repository.ChiroUnitRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -31,20 +21,15 @@ public class ChiroContactService {
      * @return JSON from Chiro
      */
     public String getContact(Integer adNumber) throws URISyntaxException {
+        // TODO: Let URL be autowired from configuration file.
         String url = "https://cividev.chiro.be/sites/all/modules/civicrm/extern/rest.php?key=2340f8603072358ffc23f5459ef92f88&api_key=vooneih8oo1XepeiduGh&entity=Light&action=getcontact&json=%7B%22adnr%22:" + adNumber + "%7D";
 
-
-        /**
-         * Throws exception when the URL isn't valid, no further checks necessary because of this.
-         */
+        // Throws exception when the URL isn't valid, no further checks necessary because of this.
         URI uri = new URI(url);
 
         RestTemplate restTemplate = new RestTemplate();
 
-        return restTemplate.getForEntity(uri,
-                    String.class).getBody();
-
-
+        return restTemplate.getForEntity(uri, String.class)
+                           .getBody();
     }
-
 }
