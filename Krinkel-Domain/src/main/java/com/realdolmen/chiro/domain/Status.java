078f2163c42e4941ca9383d37997923a42153fee
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-11-15 10:24:55 +0100

    add sync status field to RegistrationParticipant

--- Krinkel-Domain/src/main/java/com/realdolmen/chiro/domain/Status.java
+++ Krinkel-Domain/src/main/java/com/realdolmen/chiro/domain/Status.java
@@ -3,6 +3,7 @@ package com.realdolmen.chiro.domain;
 public enum Status {
     TO_BE_PAID,
     PAID,
-    CONFIRMED
+    CONFIRMED,
+    SYNCED
     //Status changes to confirmed when the confirmation link is clicked
 }
