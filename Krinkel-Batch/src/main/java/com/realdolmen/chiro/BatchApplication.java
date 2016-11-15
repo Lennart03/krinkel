d864f65c28b6f820ba52cf298f5b72e0d008e854
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-11-15 15:29:54 +0100

    only deploying rest in development will create db

--- Krinkel-Batch/src/main/java/com/realdolmen/chiro/BatchApplication.java
+++ Krinkel-Batch/src/main/java/com/realdolmen/chiro/BatchApplication.java
@@ -3,8 +3,10 @@ package com.realdolmen.chiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:batch-development-properties.properties")
 public class BatchApplication {
 
     public static void main(String[] args) {
--- Krinkel-Rest/src/main/java/com/realdolmen/chiro/Application.java
+++ Krinkel-Rest/src/main/java/com/realdolmen/chiro/Application.java
@@ -2,8 +2,11 @@ package com.realdolmen.chiro;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySource("classpath:rest-development-properties.properties")
 public class Application {
 
     public static void main(String[] args) {
