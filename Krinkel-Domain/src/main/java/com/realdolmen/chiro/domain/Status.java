e9425c392ba82c4973f3220102e4c6d7d6ac6d55
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-11-15 13:20:07 +0100

    remove unused SYNCED status

--- Krinkel-Domain/src/main/java/com/realdolmen/chiro/domain/Status.java
+++ Krinkel-Domain/src/main/java/com/realdolmen/chiro/domain/Status.java
@@ -3,7 +3,6 @@ package com.realdolmen.chiro.domain;
 public enum Status {
     TO_BE_PAID,
     PAID,
-    CONFIRMED,
-    SYNCED
+    CONFIRMED
     //Status changes to confirmed when the confirmation link is clicked
 }
