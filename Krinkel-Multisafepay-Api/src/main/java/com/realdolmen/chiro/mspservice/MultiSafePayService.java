2e03ea3d1c41ed1c7f09503be117e0ef1ad56afd
Mathias Bulté <bulte.mathias@gmail.com>
2016-11-17 09:35:33 +0100

    Added { and } to if statement which caused problems.
    DON'T WRITE IF STATEMENTS WITHOUT { AND }.

--- Krinkel-Multisafepay-Api/src/main/java/com/realdolmen/chiro/mspservice/MultiSafePayService.java
+++ Krinkel-Multisafepay-Api/src/main/java/com/realdolmen/chiro/mspservice/MultiSafePayService.java
@@ -61,14 +61,15 @@ public class MultiSafePayService {
     private boolean createPaymentParamsAreValid(String orderId, Integer amount) {
         boolean res = true;
 
-        if (orderId == null)
-        	logger.error("orderId payment = null");
+        if (orderId == null) {
+            logger.error("orderId payment = null");
             res = false;
+        }
 
-        if (amount == null || amount < 0)
-        	logger.error("amount payment = null or negative");
+        if (amount == null || amount < 0) {
+            logger.error("amount payment = null or negative");
             res = false;
-
+        }
         return res;
     }
 
