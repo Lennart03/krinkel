629e9edddef16ed3925872d9b30c06f70515fdd0
Arne Knockaert <arneknockaert@hotmail.com>
2016-12-02 09:58:41 +0100

    remake test

--- Krinkel-Rest/src/test/java/com/realdolmen/chiro/controller/UnitControllerMockitoTest.java
+++ Krinkel-Rest/src/test/java/com/realdolmen/chiro/controller/UnitControllerMockitoTest.java
@@ -2,10 +2,7 @@ package com.realdolmen.chiro.controller;
 
 import com.realdolmen.chiro.domain.RegistrationParticipant;
 import com.realdolmen.chiro.domain.RegistrationVolunteer;
-import com.realdolmen.chiro.domain.User;
 import com.realdolmen.chiro.service.ChiroUnitService;
-import com.realdolmen.chiro.service.RegistrationParticipantService;
-import com.realdolmen.chiro.service.UserService;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,8 +11,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.Mockito.times;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UnitControllerMockitoTest {
@@ -23,39 +23,40 @@ public class UnitControllerMockitoTest {
     private UnitController unitController;
 
     @Mock
-    private ChiroUnitService unitService;
-    
-    @Mock
-    private RegistrationParticipantService registrationParticipantService;
-
-    @Mock
-    private UserService userService;
+    private ChiroUnitService chiroUnitService;
 
     private final static String TEST_STAMLETTERS = "LEG ";
     private final static String TEST_STAMCIJFERS = "0000";
     private final static String TEST_STAMNR = "LEG /0000";
 
     @After
-    public void verifyStrict(){
-        Mockito.verifyNoMoreInteractions(userService);
-        //Mockito.verifyNoMoreInteractions(unitService);
+    public void verifyStrict() {
+        Mockito.verifyNoMoreInteractions(chiroUnitService);
     }
-    
+
     @Test
-    public void findRegisteredParticipantsByGroupReturnsListOfParticipants(){
-    	List<RegistrationParticipant>participants = new ArrayList<>();
-    	Mockito.when(registrationParticipantService.findParticipantsByGroup(TEST_STAMNR)).thenReturn(participants);
-    	List<RegistrationParticipant>unitParticipants = unitController.findRegisteredParticipantsByGroup(TEST_STAMNR);
-    	//Assert.assertSame(participants, unitParticipants);
-    	Assert.assertEquals(participants.size(), unitParticipants.size());
+    public void findRegisteredParticipantsByGroupReturnsListOfParticipants() {
+        List<RegistrationParticipant> participants = new ArrayList<>();
+        RegistrationParticipant registrationParticipant = new RegistrationParticipant();
+        participants.add(registrationParticipant);
+
+        Mockito.when(chiroUnitService.findParticipantsByChiroUnit(TEST_STAMNR)).thenReturn(participants);
+        List<RegistrationParticipant> unitParticipants = unitController.findRegisteredParticipantsByGroup(TEST_STAMNR);
+        Assert.assertSame(participants, unitParticipants);
+        Assert.assertEquals(participants.size(), unitParticipants.size());
+        Mockito.verify(chiroUnitService, times(1)).findParticipantsByChiroUnit(TEST_STAMNR);
     }
-    
+
     @Test
-    public void findRegisteredVolunteersByGroupReturnsListOfVolunteers(){
-    	List<RegistrationVolunteer>volunteers = new ArrayList<>();
-    	Mockito.when(registrationParticipantService.findVolunteersByGroup(TEST_STAMNR)).thenReturn(volunteers);
-    	List<RegistrationVolunteer>unitVolunteers = unitController.findRegisteredVolunteersByGroup(TEST_STAMNR);
-    	//Assert.assertSame(volunteers, unitVolunteers);
-    	Assert.assertEquals(volunteers.size(), unitVolunteers.size());
+    public void findRegisteredVolunteersByGroupReturnsListOfParticipants() {
+        List<RegistrationVolunteer> volunteers = new ArrayList<>();
+        RegistrationVolunteer registrationVolunteer = new RegistrationVolunteer();
+        volunteers.add(registrationVolunteer);
+
+        Mockito.when(chiroUnitService.findVolunteersByChiroUnit(TEST_STAMNR)).thenReturn(volunteers);
+        List<RegistrationVolunteer> unitVolunteers = unitController.findRegisteredVolunteersByGroup(TEST_STAMNR);
+        Assert.assertSame(volunteers, unitVolunteers);
+        Assert.assertEquals(volunteers.size(), unitVolunteers.size());
+        Mockito.verify(chiroUnitService, times(1)).findVolunteersByChiroUnit(TEST_STAMNR);
     }
 }
