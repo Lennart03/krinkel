ae98a0dec85f75613c147c073427c0c184c0b2ad
JeroenCompagnie <jeroen.compagnie@gmail.com>
2016-12-08 15:11:09 +0100

    controller added for exporting

---
+++ Krinkel-Rest/src/main/java/com/realdolmen/chiro/controller/ExportController.java
@@ -0,0 +1,73 @@
+package com.realdolmen.chiro.controller;
+
+import com.realdolmen.chiro.service.ExcelService;
+import com.realdolmen.chiro.service.ExportService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import sun.java2d.loops.ProcessPath;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by JCPBB69 on 8/12/2016.
+ */
+@RestController
+public class ExportController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private ExcelService excelService;
+
+    @Autowired
+    private ExportService exportService;
+
+//    @RequestMapping(value = "/download", method = RequestMethod.GET)
+//    @ResponseBody
+//    public Object downloadExcel(HttpServletResponse response) {
+//        String xlsxFileName = exportService.writeRegistrationParticipantsToXlsx();
+//
+//        response.setContentType("application/vnd.ms-excel");
+//        response.setHeader("Content-disposition",
+//                "attachment; filename=" + xlsxFileName + ".xls");
+//        try {
+//            generateExcel(response.getOutputStream());
+//        } catch (IOException e) {
+//            System.out.println("ERROR: " + e);
+//        }
+//        return null;
+//    }
+
+    @RequestMapping(value = "/exporteerVolledigeInschrijvingsLijst", method = RequestMethod.GET)
+    public void getFile(HttpServletResponse response) {
+        try {
+            // get your file as InputStream
+//            InputStream is = new InputStream();
+            String fileNameXlsx = exportService.writeRegistrationParticipantsToXlsx();
+            if(fileNameXlsx.equals("failed")){
+                logger.info("Writing xlsx file failed!");
+                return;
+            }
+
+            File file = new File(fileNameXlsx);
+
+            FileInputStream fis = new FileInputStream(file);
+            // copy it to response's OutputStream
+            org.apache.commons.io.IOUtils.copy(fis, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException ex) {
+//            log.info("Error writing file to output stream. Filename was '{}'", fileName, ex);
+            throw new RuntimeException("IOError writing file to output stream");
+        }
+
+    }
+}
