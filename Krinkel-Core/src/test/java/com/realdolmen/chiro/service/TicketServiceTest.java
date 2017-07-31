7aa24019196cb65da53b0697b7912d50b3f49b45
LennartVanDamme <lennart.vandamme@gmail.com>
2017-07-31 08:31:07 +0200

    Tests on ticket service

--- Krinkel-Core/src/test/java/com/realdolmen/chiro/service/TicketServiceTest.java
+++ Krinkel-Core/src/test/java/com/realdolmen/chiro/service/TicketServiceTest.java
@@ -2,16 +2,26 @@ package com.realdolmen.chiro.service;
 
 import com.realdolmen.chiro.config.TestConfig;
 import com.realdolmen.chiro.domain.Address;
+import com.realdolmen.chiro.domain.payments.Payment;
 import com.realdolmen.chiro.domain.payments.TicketPrice;
 import com.realdolmen.chiro.domain.payments.TicketType;
+import com.realdolmen.chiro.mspservice.MultiSafePayService;
 import com.realdolmen.chiro.payment.TicketDTO;
+import com.realdolmen.chiro.repository.PaymentRepository;
+import com.realdolmen.chiro.repository.TicketPriceRepository;
 import com.realdolmen.chiro.spring_test.SpringIntegrationTest;
+import com.realdolmen.chiro.util.TicketPriceCalculator;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @ContextConfiguration(classes = {TestConfig.class})
@@ -36,6 +46,22 @@ public class TicketServiceTest extends SpringIntegrationTest {
     @Autowired
     private TicketService ticketService;
 
+    @InjectMocks
+    private TicketService mockedTicketService;
+
+    @Mock
+    private TicketPriceRepository ticketPriceRepository;
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @Mock
+    private TicketPriceCalculator ticketPriceCalculator;
+
+    @Mock
+    private MultiSafePayService multiSafePayService;
+
+    @Before
     public void setup() {
         dto = new TicketDTO();
         dto.setType(TicketType.TREIN);
@@ -76,7 +102,24 @@ public class TicketServiceTest extends SpringIntegrationTest {
 
     @Test
     public void createPayment() throws Exception {
-        ticketService.createPayment(dto);
-
+        String paymentUrl = ticketService.createPayment(dto);
+        Assert.assertNotNull(paymentUrl);
+        System.err.println(paymentUrl);
+        Assert.assertNotEquals("", paymentUrl);
+        Assert.assertNotEquals("error", paymentUrl);
     }
+
+    @Test
+    public void createPaymentOrderDTOIsNull() throws MultiSafePayService.InvalidPaymentOrderIdException {
+        Payment payment = new Payment(dto.getType(), new BigDecimal(12.0), dto.getFirstName(), dto.getLastName(), dto.getEmail(), dto.getPhoneNumber(), dto.getAddress());
+        List<TicketPrice> ticketPrices = new ArrayList<>();
+        ticketPrices.add(new TicketPrice(1, new BigDecimal(11.50), new BigDecimal(0.50), TicketType.TREIN));
+        Mockito.when(multiSafePayService.createPayment(payment)).thenReturn(null);
+        Mockito.when(ticketPriceRepository.findByTicketType(dto.getType())).thenReturn(ticketPrices);
+        Mockito.when(ticketPriceCalculator.calculateTotalTicketPrice(new BigDecimal(11.50), 1, new BigDecimal(0.50))).thenReturn(new BigDecimal(12.0));
+        Mockito.when(paymentRepository.save(payment)).thenReturn(payment);
+        String paymentUrl = mockedTicketService.createPayment(dto);
+        Assert.assertEquals("", paymentUrl);
+    }
+
 }
